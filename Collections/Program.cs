2178e7fea6a141482bb004923848f680d99c61de
Anna Dereniuk <Anna_Dereniuk@epam.com>
2019-03-18 15:12:15 +0200

    progress

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -12,49 +12,53 @@ namespace Collections
             int value;
             string stop = "stop";
             while (true)
-            {  try
+            {
+                var enteredValue = Console.ReadLine();
+                try
                 {
-                    var enteredValue = Console.ReadLine();
                     value = Convert.ToInt32(enteredValue);
+                   
+                    if (Object.ReferenceEquals(typeof(Int32), value.GetType()))
+                    {
+                        integerItems.Add(value);
+                    }
+                    
+                }
+                catch (FormatException ex)
+                {
                     if (Object.Equals(enteredValue, stop))
                     {
                         break;
                     }
-                    else if (Object.ReferenceEquals(enteredValue.GetType(), value.GetType()))
-                    {
-                        integerItems.Add(value);
-                    }
-                    else if (!Object.ReferenceEquals(typeof(Int32), value.GetType()))
+                    else if (!Object.ReferenceEquals(typeof(Int32), enteredValue.GetType()))
                     {
                         Console.WriteLine("Entered value is not an int. Please, enter an integer value:");
                     }
                 }
-                catch(ArgumentException ex)
-                {
-                    Console.WriteLine("Smth is wrong with your input");
-                }
-
+            }
                 Console.WriteLine("Here are your values:");
                 foreach (int num in integerItems)
                 {
-                    Console.WriteLine(num + " ");
+                    Console.Write(num + " ");
                 }
 
-                Console.WriteLine("Here is a sorted descending list of your values:");
+            Console.WriteLine();
+            Console.WriteLine("Here is a sorted descending list of your values:");
                 integerItems.Sort();
-                //int min = (int)integerItems[0];
+                int min = (int)integerItems[0];
                 integerItems.Reverse();
-               // int max = (int)integerItems[0];
+                int max = (int)integerItems[0];
                 foreach (int num in integerItems)
                 {
-                    Console.WriteLine(num + " ");
+                    Console.Write(num + " ");
                 }
-                
-               // Console.WriteLine("Here is your short list:");
-               // Console.WriteLine(max + ", " + min);
+
+            Console.WriteLine();
+            Console.WriteLine("Here is your short list:");
+                Console.Write(max + ", " + min);
                 Console.ReadKey();
             }
 
         }
     }
-}
+
